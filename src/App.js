54750d58f373aebab432752c06935cee149d5ffb
Jiyoh Macbook <geovannih514@gmail.com>
2023-09-30 19:53:00 -0700

    file structure done & components are all rendering as outline

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Project from './components/Project';
+import Footer from './components/Footer';
+
+function App() {
+    return (
+        <div className="react-portfolio">
+            <Header/>
+            <Nav/>
+            <Project/>
+            <Footer/>
+        </div>
+    )
+};
+
+export default App;
