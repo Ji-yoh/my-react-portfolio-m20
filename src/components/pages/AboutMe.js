c371c67be51a730a54eb19846a5eca6b2ab57ec0
Jiyoh Macbook <geovannih514@gmail.com>
2023-11-30 13:08:47 -0800

    updated about me

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -5,8 +5,8 @@ function AboutMe() {
         <Container>
             <div className="about-me">
                 <h2>About Me</h2>
-                <p>Lifelong learning advocate and 10 year banking veteran just starting a new journey into web development. Current student with the UC Berkeley Extension/EDX Full-stack Web Development Bootcamp on track to graduate October 2023.</p>
-                <p>Interested in learning new programming languages beyond the bootcamp and in the semi-long term hope to work on solutions to detecting deep-fake media, as well as expand my Cybersecurity knowledge.</p>
+                <p>Lifelong learning advocate and 10 year banking veteran just starting a new journey into web development. Graduated the UC Berkeley Extension/EdX full-stack web development bootcamp in October 2023 and continuing to learn!</p>
+                <p>Interested in learning new programming languages beyond the bootcamp and gain new data analysis skills. In the semi-long term I hope to work on solutions to detecting deep-fake media.</p>
                 <p>Also a current student at Arizona State University with plans to graduate in Fall 2024 with a B.A. in Technical Communication. Open to all opportunities, feel free to reach out!</p>
             </div>
         </Container>
